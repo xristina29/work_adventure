d378add290a79f7eaca6da58cdb5ff6bdce39855
xristina29 <49676223+xristina29@users.noreply.github.com>
2021-02-16 21:36:56 +0100

    casle and co

---
+++ castle.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="castle" tilewidth="32" tileheight="32" tilecount="152" columns="8">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="tiles/castle.png" width="256" height="608"/>
+</tileset>
