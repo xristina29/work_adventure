9226a4b0bd7c3465ba3cbe81ae5eb400e8f781e8
xristina29 <49676223+xristina29@users.noreply.github.com>
2021-02-16 20:25:57 +0000

    Deploying to gh-pages from master @ d7bc29911394eaf3297162c757ca3daea2bf7fb3 🚀

---
+++ upholstery.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="upholstery" tilewidth="32" tileheight="32" tilecount="512" columns="16">
+ <image source="tiles/upholstery.png" width="512" height="1024"/>
+ <tile id="0">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
