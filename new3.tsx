c35c152cabd6153e4127c50df58772f5fa807e49
xristina29 <49676223+xristina29@users.noreply.github.com>
2021-02-21 20:22:41 +0000

    Deploying to gh-pages from master @ 28c7988db04b9374ee21a1da7bc4f956de7c3f46 🚀

--- new3.tsx
+++ new3.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="new3" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
  <image source="tiles/Atlas2/build_atlas.png" width="1024" height="1024"/>
+ <tile id="283">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="284">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="315">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="316">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="347">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="348">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
